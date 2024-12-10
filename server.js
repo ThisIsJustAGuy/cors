ce46efa45cb228a491ebf7e9dbfb3c6ab34e3ef5
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-12-10 15:40:42 +0100

    change

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
+
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
+var request = require('request');
+var express = require('express');
+var app = express();
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-originWhitelist.push('https://calendar.zoho.eu');
-originWhitelist.push('https://zohoapis.eu');
-originWhitelist.push('http://localhost:4200');
 function parseEnvList(env) {
   if (!env) {
     return [];
   }
   return env.split(',');
 }
+
+
+app.use('/zoho', function(req, res){
+  var accessToken = req.params.access_token;
+  var zohoUId = req.params.zohoUId;
+  var param = req.params.param;
+  var zohoUrl = 'https://calendar.zoho.eu/api/v1/calendars/' + zohoUId + '/events?' + param;
+
+  var options = {
+    url: zohoUrl,
+    headers: {
+      'Authorization' : 'Bearer ' + accessToken,
+    },
+  };
+
+  request(options).on('response', function (response){
+    res.status(response.statusCode);
+  }).pipe(res);
+});
+
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
